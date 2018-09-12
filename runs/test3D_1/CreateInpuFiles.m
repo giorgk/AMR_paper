73da4e8f5bbc37b9364f5c3340db62d6dd05b3bd
George Kourakos <giorgk@gmail.com>
2018-09-12 15:21:01 -0700

    create streams on the 3D example

--- runs/test3D_1/CreateInpuFiles.m
+++ runs/test3D_1/CreateInpuFiles.m
@@ -1,9 +1,27 @@
 % This script will create the input files for the first 3D test which will
 % be included in the paper
 %
+msim_path = 'home/giorgk/CODES/msim';
 % The domain a rectangle with dimensions 5 x 5 km.
 L = 5000;
-% The recharge rate is 
+dom = [0 0; L 0; L L; 0 L];
+
+%% load stream from the NPSAT tutorial
+strm = shaperead([msim_path '/html_help/DATA/Streams_exampl_buff']);
+% the stream ids inside the domain of this example are 
+% The recharge rate is 14-18.
+% The segment 14 needs trim on the top
+strm(14,1).Y(strm(14,1).Y > 5000) = L;
+strm(17,1).Y(strm(17,1).Y < 0) = 0;
+strm = strm(14:18,:);
+for ii = 1:size(strm,1)
+    strm(ii,1).area = polyarea(strm(ii,1).X(1:end-1), strm(ii,1).Y(1:end-1));
+end
+%% write the stream file
+fid = fopen('test3D_stream.npsat','w');
+%fprintf(fid,
+fclose(fid);
+%%
 rch = 0.0004;
 % Therefore, the total recharge volume is
 RCV_vol = rch*L*L;
