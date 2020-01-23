b59c85e3451d9963c3c69eda7f2c173839eb5807
George Kourakos <giorgk@gmail.com>
2020-01-22 23:48:11 -0800

    Create boundary geometry file

--- runs/testValid/PrepareNPSATfiles.m
+++ runs/testValid/PrepareNPSATfiles.m
@@ -1,3 +1,14 @@
 % Write mesh file
+Radius = 2000;
 [p, MSH] = readOBJmesh('Circle_zbrush.OBJ', 4);
-writeMeshfile('circle_mesh.npsat', 2000*p(:,1:2), MSH)
+p = Radius*p;
+writeMeshfile('circle_mesh.npsat', p(:,1:2), MSH)
+
+[bnd_p, bnd_MSH] = readOBJmesh('DC_BND_file.obj', 40);
+
+
+
+fid = fopen('CH_BND.npsat','w');
+fprintf(fid, 'BOUNDARY_LINE\n');
+
+
